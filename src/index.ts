773da4ef68dd1018223b78cd8469913a1f5aa260
leumasic <53974949+leumasic@users.noreply.github.com>
2024-06-01 09:44:58 -0400

    add jsdoc and fix README

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
+/**
+ * Factory for creating invariant functions that throw custom exceptions.
+ *
+ * Takes an `exceptionProducer` function that handles the 2nd argument of the
+ * resulting `invariant` function and returns the exception to throw.
+ *
+ * @important Type-annotating the resulting `invariant` function is mandatory
+ * because it is an assertion function.
+ *
+ * @example
+ *
+ * ```ts
+ * const exceptionProducer = (exceptionData: string) => new CustomError(exceptionData)
+ * const invariant: (
+ *    condition: any,
+ *    exceptionData: Parameters<typeof exceptionProducer>[0]
+ * ) => asserts condition = invariantFactory(exceptionProducer)
+ *
+ * invariant(falsyValue, 'Expected condition to be truthy')
+ * // CustomError('Expected condition to be truthy')
+ * ```
+ */
 export function invariantFactory<T, K>(
   exceptionProducer: (exceptionData: T) => K extends Promise<any> ? never : K,
 ) {
@@ -8,6 +30,28 @@ export function invariantFactory<T, K>(
   }
 }
 
+/**
+ * Factory for creating async invariant functions that throw custom exceptions.
+ *
+ * Takes an async `exceptionProducer` function that handles the 2nd argument of
+ * the resulting `invariant` function and returns the exception to throw.
+ *
+ * @important Type-annotating the resulting `invariant` function is mandatory
+ * because it is an assertion function.
+ *
+ * @example
+ *
+ * ```ts
+ * const asyncExceptionProducer = async (exceptionData: string) => new CustomError(exceptionData)
+ * const invariant: (
+ *    condition: any,
+ *    exceptionData: Parameters<typeof asyncExceptionProducer>[0]
+ * ) => asserts condition = asyncInvariantFactory(asyncExceptionProducer)
+ *
+ * await invariant(falsyValue, 'Expected condition to be truthy')
+ * // CustomError('Expected condition to be truthy')
+ * ```
+ */
 export const asyncInvariantFactory = <T, K>(
   exceptionProducer: (exceptionData: T) => Promise<K>,
 ) => {
@@ -26,6 +70,21 @@ const defaultExceptionProducer = (exceptionData?: string | (() => string)) => {
   return new Error(exceptionData)
 }
 
+/**
+ * Asserts that `condition` is truthy, if it is.
+ *
+ * Otherwise, throws an `Error` with the `message` field set to
+ * - `exceptionData` (or its return value) if it is passed,
+ * - `'Invariant error'` if it isn't.
+ *
+ * @example
+ *
+ * ```ts
+ * const username: string | undefined = 'leumasic'
+ * invariant(username, 'Username is required')
+ * // type of `username` is narrowed to `string`
+ * ```
+ */
 export const invariant: (
   condition: any,
   exceptionData?: Parameters<typeof defaultExceptionProducer>[0],
